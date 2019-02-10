8a8d953877a859c4d83691cab21f0b4c71b8241a
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-10 16:52:09 -0500

    Establish some global styles

--- client/src/components/Sections/Contact.js
+++ client/src/components/Sections/Contact.js
@@ -6,6 +6,7 @@ const Contact = props => {
     return (
         <>
         <h2>Contact</h2>
+        <p>Let's connect. <a href="mailto:leigh.ann.friedel@gmail.com">leigh.ann.friedel@gmail.com</a></p>
         {social.map(account => (
             <SocialLink account={account} key={account.account} />
         ))}
--- client/src/styles/Global.js
+++ client/src/styles/Global.js
@@ -1,7 +1,40 @@
 import { css } from 'styled-components';
 
 export const Global = css`
-h1, h2, h3 {
+html {
+    font-size: 62.5%;
+    font-family: 'Montserrat', sans-serif;
+}
+
+h2, h3 {
     font-weight: 700;
+    margin-bottom: 20px;
+}
+
+h1 {
+    font-weight: 900;
+    font-size: 9rem;
+    margin-bottom: 40px;
+}
+
+h2 {
+    font-size: 1.3rem;
+    text-transform: uppercase;
+}
+
+h3 {
+    font-size: 2rem;
+}
+
+p, a, span {
+    font-size: 1.8rem;
+    margin-bottom: 8px;
+}
+
+button {
+    padding: 8px 12px;
+    font-family: 'Montserrat', sans-serif;
+    text-transform: uppercase;
+    font-size: 1rem;
 }
 `;
