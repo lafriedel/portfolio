47fbd2d113eebaa4b72b55328e4c74551c536e59
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-10 15:33:20 -0500

    Boilerplate global styles and reset with styled-components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Home } from './components/Home';
+import { GlobalStyles } from './styles';
 
 class App extends Component {
   render() {
     return (
       <>
+        <GlobalStyles />
         <Home />
       </>
     );
