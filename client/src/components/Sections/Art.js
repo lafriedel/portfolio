9a1970dd318f85b6e11a0542f95ba3536b9144ac
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-06-15 22:29:44 -0400

    Remove art project button for now

--- client/src/components/Sections/Art.js
+++ client/src/components/Sections/Art.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { ArtProject } from "../Art";
+import art from "../../art-data";
 
 const Art = () => {
   return (
     <>
-    <div className="section" id="projects-section">
-    <h2>Art</h2>
-  </div>
-  </>
-  )
-}
+      <div className="section" id="projects-section">
+        <h2>Art</h2>
+      </div>
+      {art.map(project => (
+        <ArtProject project={project} key={project.id} />
+      ))}
+    </>
+  );
+};
 
 export default Art;
