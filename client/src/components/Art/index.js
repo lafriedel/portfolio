10f9dc766d374ee10e88fe7e7b1f1a20f763ebda
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2023-11-09 08:09:46 -0800

    Delete unused files

--- client/src/components/Art/index.js
+++
@@ -1,1 +0,0 @@
-export { default as ArtProject } from "./ArtProject";
--- client/src/components/Sections/index.js
+++ client/src/components/Sections/index.js
@@ -1,7 +1,3 @@
-export { default as Navigation } from "./Navigation";
 export { default as Header } from "./Header";
 export { default as About } from "./About";
-export { default as Skills } from "./Skills";
-export { default as Projects } from "./Projects";
-export { default as Art } from "./Art";
 export { default as Contact } from "./Contact";
