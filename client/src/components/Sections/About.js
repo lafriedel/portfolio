8b60a1b672516440620e682bba7c8fbd03e21977
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2024-05-30 19:10:58 -0700

    Update language on main page

--- client/src/components/Sections/About.js
+++ client/src/components/Sections/About.js
@@ -5,7 +5,7 @@ const About = props => {
   return (
     <AboutContainer>
       <p>
-        Hello, I'm Leigh-Ann! Thanks for visiting my tiny corner of the web. 
+        Hello, I'm Leigh-Ann! Thanks for visiting my corner of the internet. 
       </p>
       <p>
         I am a Seattle-based <span>full-stack software engineer</span> with a background in experiential design. 
@@ -14,7 +14,10 @@ const About = props => {
         Since mid-2019 I've been working at <a target="_blank" rel="noopener noreferrer" href="https://tackle.io">Tackle.io</a>, helping software companies accelerate their revenue in the cloud marketplaces.
       </p>
       <p>
-        When I'm not developing software you can likely find me birding, printmaking, doing aerial yoga, riding my bike, or reading about personal finance.
+        I am also an <span>artist and printmaker</span>. I sell my work in my <a target="_blank" rel="noopener noreferrer" href="https://shop.leighannfriedel.com">online shop</a> and in person at my Seattle-based studio.
+      </p>
+      <p>
+        When I'm not developing software or making art, you can likely find me birding, doing aerial yoga, or spending time with my husband and cat.
       </p>
     </AboutContainer>
   );
